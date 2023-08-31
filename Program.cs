0f4871964acc45276a464f7390cd28d80ce4fd39
Emil Sondum <eson@itu.dk>
2023-08-31 15:54:51 +0200

    Reading from csv file is now possible.
    Some structor for furture is added.
    
    Co-authored-by: Thøger bros@itu.dk
    Co-authored-by: Lukas lgul@itu.dk
    Co-authored-by: Tobias toen@itu.dk
    Co-authored-by: Rafael rafj@itu.dk

--- Program.cs
+++ Program.cs
@@ -1,6 +1,55 @@
-﻿using System.Collections;
+﻿using System.Text.RegularExpressions;
 
-foreach (var arg in args)
+if (args.Length == 0)
 {
-    Console.WriteLine(arg);
+    Console.WriteLine("Missing arguments");
+    return;
+}
+
+switch (args[0])
+{
+    case "read":
+        //Read all Cheeps from file
+        readFile();
+        printCheeps();
+        //Print out all Cheeps
+        break;
+    case "cheep":
+        addCheep(args[1]);
+        //Write input as a new Cheep to file
+        break;
+    default:
+        Console.WriteLine("Invalid argument");
+        return;
+}
+
+void readFile()
+{
+    try
+    {
+        using var sr = new StreamReader("chirp_cli_db.csv");
+        while (!sr.EndOfStream)
+        {
+            // Regex adapted from https://stackoverflow.com/questions/3507498/reading-csv-files-using-c-sharp/
+            var line = sr.ReadLine();
+            var CSVParser = new Regex(",(?=(?:[^\"]*\"[^\"]*\")*(?![^\"]*\"))");
+
+            //Separating columns to array
+            string[] splitLine = CSVParser.Split(line);
+            // create cheep from splitLine, and add to cheep list 
+        }
+    }
+    catch (System.Exception)
+    {
+
+        throw;
+    }
+}
+
+void printCheeps() {
+    // not implemented yet
+}
+
+void addCheep(string message) {
+    // not implemented yet
 }
