8763e99554f746fe2f58a65889d23a7d51b1cfea
Emil Sondum <eson@itu.dk>
2023-09-05 13:06:32 +0200

    Enabled printing and saving of Cheeps
    Cheep class made, with ToString metods.
    
    Co-authored-by: Thøger bros@itu.dk

---
+++ Cheep.cs
@@ -0,0 +1,29 @@
+public class Cheep{
+    string message;
+    string author;
+    DateTimeOffset messageTime;
+
+    //Used when creating a new Cheep and writing to CSV file
+    public Cheep(string author, string message, DateTimeOffset timeOfMessage){
+
+        this.author = author;
+        this.message = message;
+        this.messageTime = timeOfMessage;
+    }
+
+    //Used when reading Cheep from CSV file
+    public Cheep(string author, string message, long unixTime){
+        this.author = author;
+        this.message = message;
+        this.messageTime = DateTimeOffset.FromUnixTimeSeconds(unixTime);
+    }
+
+
+    public string ToCsvString(){
+        return $"{author},{message},{messageTime.ToUnixTimeSeconds()}";
+    }
+
+    public override string ToString(){
+        return $"{author} @ {messageTime:dd/mm/yy HH:mm:ss}: {message}";
+    }
+}
