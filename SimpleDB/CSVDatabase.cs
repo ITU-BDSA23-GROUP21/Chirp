a1ee72ff2994f533506d26b211f362594c90d675
Rafael <rafj@itu.dk>
2023-09-12 13:03:42 +0200

    Extended CSVDatabase.cs with constructor and store method

--- SimpleDB/CSVDatabase.cs
+++ SimpleDB/CSVDatabase.cs
@@ -1,10 +1,29 @@
+using System.Globalization;
+using CsvHelper;
+using CsvHelper.Configuration;
 
-public sealed class CSVDatabase<T> : IDatabaseRepository<T> {
-    public IEnumerable<T> Read(int limit) {
+public sealed class CSVDatabase<T> : IDatabaseRepository<T>{
+    private string pathToCSV;
 
+    public CSVDatabase(string pathToCSV)
+    {
+        this.pathToCSV = pathToCSV;
     }
 
-    public void Store(T record) {
+    public IEnumerable<T> Read(int limit){
+        
+    }
 
+    public void Store(T record){
+        CsvConfiguration config = new CsvConfiguration(CultureInfo.InvariantCulture)
+        {
+            HasHeaderRecord = false,
+        };
+        using(var stream = File.Open(pathToCSV, FileMode.Append))
+        using (var writer = new StreamWriter(stream))
+        using (var csv = new CsvWriter(writer, config))
+        {
+            csv.WriteRecord(record);
+        }
     }
 }
