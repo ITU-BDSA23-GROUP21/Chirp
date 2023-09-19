70cc1f113391e6c1a4f31fb6cccf06c7056bb160
lucas <lgul@itu.dk>
2023-09-19 13:50:41 +0200

    testAddCheep working with reading csv file

--- test/Chirp.CLI-test/TestAddCheep.cs
+++ test/Chirp.CLI-test/TestAddCheep.cs
@@ -1,7 +1,6 @@
 //integration test
 namespace Chirp.CLI_test {
-    public class TestAddCheep
-    {
+    public class TestAddCheep {
         #region Sample_TestCode
         [Theory]
         [InlineData("name1", "generic test", 111)] //data to be tested in genericTest()
@@ -10,15 +9,28 @@ namespace Chirp.CLI_test {
 
         static void genericTest(String name, String message, long timestamp) {
 
+            //IDatabaseRepository<Cheep> database = new CSVDatabase<Cheep>("../../../../../data/chirp_cli_db.csv");
 
             DateTimeOffset dateTime = DateTimeOffset.FromUnixTimeSeconds(timestamp).ToLocalTime(); //converting input long to dateTime
             Cheep cp1 = new Cheep(name, message, timestamp); //making cheep to be tested
-            List<Cheep> cheeps = new List<Cheep>{cp1}; // inserting cheep in iterable list
+            List<Cheep> cheeps = new List<Cheep> { cp1 }; // inserting cheep in iterable list
 
             String expected = $"{name}, {timestamp}, \"{message}\""; //making the expected result
-            String actual = File.ReadLines("../../data/chirp_cli_db.csv");
+            //string actual =  File.ReadLines("data/chirp_cli_db.csv/").Last();
+            String actual = new String("");
 
-            Assert.Equal(expected, actual)
+            Program pr = new Program();
+
+
+
+            Program.setDB();
+            /*Program.AddCheep(message); */
+            Program.AddCheep(message);
+            actual = File.ReadLines("../../../../../data/chirp_cli_db.csv").Last();
+
+
+
+            Assert.Equal(expected, actual);
         }
         #endregion
     }
