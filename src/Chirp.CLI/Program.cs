d82584f069de634dba5fdb5b4485be586b2d62a8
rafj58 <143702988+rafj58@users.noreply.github.com>
2023-09-19 12:42:15 +0200

    Commandline arguments iss21 (#32)
    * Refactored code to follow established conventions and added argument to specify amount of cheeps to read
    
    * Deleted unnecessary method
    
    * Completed functionality to specify amount of cheeps to read and reversed the order of which the cheeps are printed so the latest is first
    
    * Refactored code to follow established conventions and added argument to specify amount of cheeps to read
    
    * Deleted unnecessary method
    
    * Completed functionality to specify amount of cheeps to read and reversed the order of which the cheeps are printed so the latest is first

--- src/Chirp.CLI/Program.cs
+++ src/Chirp.CLI/Program.cs
@@ -15,40 +15,40 @@ internal class Program {
             args = new[] { "--help" };
         }
 
-        // Define available options for CLI input
-        var readOption = new Option<bool?>(
-            name: "read",
-            description: "Read chirps from the database"
-            ) {
-            // Disallow typing a value for "read", as cli library crashes if parsing to boolean fails
-            Arity = ArgumentArity.Zero
-        };
+        //The rootcommand
+        RootCommand rootCommand = new RootCommand("Welcome to Cheep. Get Chirping!");
+        rootCommand.SetHandler(() => { Console.WriteLine("Welcome to Cheep. Get Chirping!"); });
 
-        var cheepOption = new Option<string>(
-            name: "cheep",
-            description: "Send a new cheep"
+        //Subcommand read and arguments
+        Command readCommand = new Command("read", "Read cheeps.");
+        rootCommand.AddCommand(readCommand);
+
+        Argument<int> amountArgument = new Argument<int>(
+            name: "amount",
+            description: "Specifies the amount of cheeps to show",
+            getDefaultValue: () => 5
         );
-        var rootCommand = new RootCommand("Welcome to Cheep. Get Chirping.");
-        rootCommand.AddOption(cheepOption);
-        rootCommand.AddOption(readOption);
+        readCommand.AddArgument(amountArgument);
+        readCommand.SetHandler(PrintCheeps, amountArgument);
 
-        // Define handling of CLI input
-        rootCommand.SetHandler(HandleCommands, readOption, cheepOption);
+        //Subcommand cheep and arguments
+        Command cheepCommand = new Command("cheep", "Cheep a message.");
+        rootCommand.AddCommand(cheepCommand);
+
+        Argument<string> messageArgument = new Argument<string>(
+            name: "message",
+            description: "The message to cheep."
+        );
+        cheepCommand.AddArgument(messageArgument);
+        cheepCommand.SetHandler(AddCheep, messageArgument);
 
         // Invoke handler with input args
         return await rootCommand.InvokeAsync(args);
     }
 
-    private static void HandleCommands(bool? read, string cheep) {
-        // You can currently read and cheep at the same time. Is this intended?
-        if (read == true) {
-            var cheeps = CSVdb.Read();
-            UserInterface.PrintCheeps(cheeps);
-        }
-
-        if (!string.IsNullOrEmpty(cheep)) {
-            AddCheep(cheep);
-        }
+    private static void PrintCheeps(int amount) {
+        IEnumerable<Cheep> cheeps = CSVdb.Read();
+        UserInterface.PrintCheeps(cheeps, amount);
     }
 
     static private void AddCheep(string message) {
