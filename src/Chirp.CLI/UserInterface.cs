9e6dad737adb69c8427a9861f6a7954dc4f1cded
lucas <lgul@itu.dk>
2023-09-24 00:38:59 +0200

    GET & POST working with main method in client

--- src/Chirp.CLI/UserInterface.cs
+++ src/Chirp.CLI/UserInterface.cs
@@ -1,5 +1,5 @@
 public static class UserInterface {
-    public static async void PrintCheeps(List<Cheep> cheeps) {
+    public static void PrintCheeps(List<Cheep> cheeps) {
         foreach (Cheep cheep in cheeps)
             Console.WriteLine(cheep.ToString());
     }
