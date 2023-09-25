fecb3934c1494c1deabdf5b5e66711740e88383b
Thøger Bro <bros@example.com>
2023-09-25 15:13:27 +0200

    temp remove test broken by using web API

--- src/Chirp.CSVDBService/Program.cs
+++ src/Chirp.CSVDBService/Program.cs
@@ -4,6 +4,9 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 var cheepHandler = new CheepHandler();
 
+// TODO: Add basic exception handling
+// i.e. return error 500 instead of crashing API
+
 // Fetch all cheeps and return as JSON    
 app.MapGet("/cheeps", (int amount) => cheepHandler.GetCheeps(amount));
 
