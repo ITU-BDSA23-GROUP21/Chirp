6816628a99a47fe490635121fbd8241cf0fe1a3f
Thøger Bro <bros@itu.dk>
2023-09-26 11:20:29 +0200

    optional query param in api
    Co-authored-by: Tobias <toen@itu.dk>
    Co-authored-by: Lucas <lgul@itu.dk>
    Co-authored-by: Emil <eson@itu.dk>

--- src/Chirp.CSVDBService/Program.cs
+++ src/Chirp.CSVDBService/Program.cs
@@ -8,7 +8,7 @@ var cheepHandler = new CheepHandler();
 // i.e. return error 500 instead of crashing API
 
 // Fetch all cheeps and return as JSON    
-app.MapGet("/cheeps", (int amount) => cheepHandler.GetCheeps(amount));
+app.MapGet("/cheeps", (int? amount) => cheepHandler.GetCheeps(amount));
 
 // Post a cheep into the database
 app.MapPost("/cheep", (Cheep cheep) => cheepHandler.AddCheep(cheep.Message, cheep.Author));
