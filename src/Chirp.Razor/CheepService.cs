e0ba72811289e6101a77c3dcbfe54990afce6cac
Thøger Bro <bros@itu.dk>
2023-10-03 12:17:58 +0200

    add connection to sqlite DB
    Co-authored-by: Tobias <toen@itu.dk>
    Co-authored-by: Emil <eson@itu.dk>

--- src/Chirp.Razor/CheepService.cs
+++ src/Chirp.Razor/CheepService.cs
@@ -8,30 +8,18 @@ public interface ICheepService
 
 public class CheepService : ICheepService
 {
-    // These would normally be loaded from a database for example
-    private static readonly List<CheepViewModel> _cheeps = new()
-        {
-            new CheepViewModel("Helge", "Hello, BDSA students!", UnixTimeStampToDateTimeString(1690892208)),
-            new CheepViewModel("Rasmus", "Hej, velkommen til kurset.", UnixTimeStampToDateTimeString(1690895308)),
-        };
+    private readonly IDBFacade dBFacade;
+
+    public CheepService(IDBFacade _dBFacade) => dBFacade = _dBFacade;
 
     public List<CheepViewModel> GetCheeps()
     {
-        return _cheeps;
+        return dBFacade.GetCheeps();
     }
 
     public List<CheepViewModel> GetCheepsFromAuthor(string author)
     {
         // filter by the provided author name
-        return _cheeps.Where(x => x.Author == author).ToList();
+        return dBFacade.GetCheeps(author);
     }
-
-    private static string UnixTimeStampToDateTimeString(double unixTimeStamp)
-    {
-        // Unix timestamp is seconds past epoch
-        DateTime dateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
-        dateTime = dateTime.AddSeconds(unixTimeStamp);
-        return dateTime.ToString("MM/dd/yy H:mm:ss");
-    }
-
 }
