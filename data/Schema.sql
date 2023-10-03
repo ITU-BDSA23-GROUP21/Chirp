ed68e1358993f9b4dbddb42f5343d9a8452ed679
Emil Sondum <eson@itu.dk>
2023-10-03 11:02:18 +0200

    SQLiteDB made from template

---
+++ data/Schema.sql
@@ -0,0 +1,16 @@
+-- schema taken from https://github.com/itu-bdsa/lecture_notes/blob/main/sessions/session_05/data/schema.sql
+drop table if exists user;
+create table user (
+  user_id integer primary key autoincrement,
+  username string not null,
+  email string not null,
+  pw_hash string not null
+);
+
+drop table if exists message;
+create table message (
+  message_id integer primary key autoincrement,
+  author_id integer not null,
+  text string not null,
+  pub_date integer
+);
