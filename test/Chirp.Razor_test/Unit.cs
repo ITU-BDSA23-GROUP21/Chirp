ce78179b42a6766256ba385950fd87387db84e7e
Thøger Bro <bros@itu.dk>
2023-10-05 14:42:26 +0200

    fix test case

--- test/Chirp.Razor_test/Unit.cs
+++ test/Chirp.Razor_test/Unit.cs
@@ -1,34 +1,26 @@
 namespace Chirp.Razor_test;
 
 using Microsoft.Extensions.DependencyInjection;
-public class Unit
-{
+public class Unit {
 
 
     [Theory]
     [InlineData(0, null)]
-    public static void GetFirstCheepOnPage(int page, string auther)
-    {
-        // var builder = WebApplication.CreateBuilder();
-        // builder.Services.AddSingleton<IDBFacade, DBFacade>();
-        // var sp = builder.BuildServiceProvider();
-        // DBFacade mockFacade = sp.GetService<IDBFacade>();
+    public static void GetFirstCheepOnPage(int page, string auther) {
+        // This test is dangerous, as it will fail when we start adding new cheeps
 
         IDBFacade mockFacade = new DBFacade();
         List<CheepViewModel> cheeps = mockFacade.GetCheeps(page, auther);
         CheepViewModel firstCheep = cheeps[0];
 
-        Assert.Equal("Helge", firstCheep.Author);
-        Assert.Equal("Hello, BDSA students!", firstCheep.Message);
-        Assert.Equal("08/01/23 12:16:48", firstCheep.Timestamp);
+        Assert.Equal("Jacqualine Gilcoine", firstCheep.Author);
+        Assert.Equal("Starbuck now is what we hear the worst.", firstCheep.Message);
+        Assert.Equal("08/01/23 13:17:39", firstCheep.Timestamp);
 
     }
 
-
-
-
-    public static void GetLastCheep(string author)
-    {
+    [Theory]
+    public static void GetLastCheep(string author) {
 
     }
 
