5d53b85255fe45f8f6c566ee2dc032e20f967cc0
Rafael <rafj@itu.dk>
2023-10-10 12:08:06 +0200

    Added Another Integration test
    Co-authored-by: Lucas <lgul@itu.dk>

--- test/Chirp.Razor_test/Integration.cs
+++ test/Chirp.Razor_test/Integration.cs
@@ -1,5 +1,6 @@
 using Chirp.Core;
 using Chirp.Infrastructure;
+using System.Collections.Generic;
 
 namespace Chirp.Razor_test;
 public class Integration
@@ -7,7 +8,7 @@ public class Integration
     [Theory]
     [InlineData(0)]
     [InlineData(4)]
-    public static void CheepService_GetCheeps_Return32Cheeps(int page) {
+    public static async void CheepService_GetCheeps_Return32Cheeps(int page) {
         //Arrange
         int expectedValue = 32;
         // This test is dangerous, as it will fail when we start adding new cheeps
@@ -15,7 +16,7 @@ public class Integration
         CheepService cheepService = new(new CheepRepository());
 
         //Act
-        IEnumerable<CheepDto> cheeps = cheepService.GetCheeps(page);
+        IEnumerable<CheepDto> cheeps = await cheepService.GetCheeps(page);
         int actualValue = cheeps.Count();
 
         //Assert
@@ -25,15 +26,25 @@ public class Integration
     [Theory]
     [InlineData(0)]
     [InlineData(-1)]
-    public static void CheepService_GetCheeps_ZeroAndBelowParameterValues(int page)
+    public static async void CheepService_GetCheeps_ZeroAndBelowParameterValues(int page)
     {
         //Arrange
         int expectedCheepAmount = 32;
-        CheepDto expectedFirstCheep = new("Helge", "Hello", );
-        CheepDto expectedLastCheep;
+        CheepDto expectedFirstCheep = new("Helge", "Hello", "08/");
+        CheepDto expectedLastCheep = new("", "", "");
 
-
+        Environment.SetEnvironmentVariable("CHIRPDBPATH", "./TestChirp.db");
+        CheepService cheepService = new(new CheepRepository());
         //Act
+        IEnumerable<CheepDto> cheeps = await cheepService.GetCheeps(page);
+        int      actualCheepAmount = cheeps.Count();
+        CheepDto actualFirstCheep = cheeps.First();
+        CheepDto actualLastCheep = cheeps.Last();
 
+
+        //Assert
+        Assert.Equal(expectedCheepAmount, actualCheepAmount);
+        Assert.Equal(expectedFirstCheep, actualFirstCheep);
+        Assert.Equal(expectedLastCheep, actualLastCheep);
     }
 }
