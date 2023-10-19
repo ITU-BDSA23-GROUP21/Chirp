c1f4cee5c61d84228b0f2f9909a92b1ff57afbed
Thøger Bro <bros@example.com>
2023-10-19 14:26:14 +0200

    create method for author / cheeps
    Co-authored-by: Tobias <toen@itu.dk>
    Co-authored-by: Emil <eson@itu.dk>

--- src/Chirp.Infrastructure/Repositories/AuthorRepository.cs
+++ src/Chirp.Infrastructure/Repositories/AuthorRepository.cs
@@ -1,4 +1,5 @@
 using Chirp.Core;
+using Microsoft.EntityFrameworkCore;
 
 namespace Chirp.Infrastructure;
 
@@ -7,14 +8,20 @@ public class AuthorRepository : IAuthorRepository {
     private readonly ChirpContext _dbContext;
 
     public AuthorRepository(ChirpContext dbContext) => _dbContext = dbContext;
-    
+
     public Task<AuthorDto> GetAuthorByName(string name) {
-        throw new NotImplementedException();
+        return _dbContext.Authors
+            .Where(author => author.Name == name)
+            .Select(author => new AuthorDto(author.Name, author.Email))
+            .FirstAsync();
     }
     public Task<AuthorDto> GetAuthorByEmail(string email) {
-        throw new NotImplementedException();
+        return _dbContext.Authors
+            .Where(author => author.Email == email)
+            .Select(author => new AuthorDto(author.Name, author.Email))
+            .FirstAsync();
     }
-    public Task CreateAuthor(AuthorDto author) {
-        throw new NotImplementedException();
+    public async Task CreateAuthor(AuthorDto author) {
+        await _dbContext.Authors.AddAsync(new Author() {Id = Guid.NewGuid(), Email = author.Email, Name = author.Name, Cheeps = new List<Cheep>()});
     }
 }
