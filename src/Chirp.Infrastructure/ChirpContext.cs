958defbeb8a165688fd901c745dc9dfb5a669a6a
Thøger Bro <bros@itu.dk>
2023-10-24 11:55:32 +0200

    set db path in program startup

--- src/Chirp.Infrastructure/ChirpContext.cs
+++ src/Chirp.Infrastructure/ChirpContext.cs
@@ -6,16 +6,6 @@ public class ChirpContext : DbContext {
     public DbSet<Author> Authors { get; set; }
     public DbSet<Cheep> Cheeps { get; set; }
 
-    public string DbPath { get; }
-
-    public ChirpContext() {
-        DbPath = Environment.GetEnvironmentVariable("CHIRPDBPATH") 
-            ?? Path.Combine(Path.GetTempPath(), "chirp.db");
-    }
-
-    protected override void OnConfiguring(DbContextOptionsBuilder options)
-            => options.UseSqlite($"Data Source={DbPath}");
-
     protected override void OnModelCreating(ModelBuilder modelBuilder) {
         modelBuilder.Entity<Cheep>().Property(c => c.Text).HasMaxLength(160);
         modelBuilder.Entity<Author>().Property(a => a.Email).HasMaxLength(60);
