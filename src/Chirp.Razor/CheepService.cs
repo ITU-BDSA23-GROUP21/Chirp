a2f480d18e00ca0fe64f7a41a17e8f04288bd033
lucas <lgul@itu.dk>
2023-11-04 14:39:36 +0100

    changed NewCheep page form to be inside layout -- have begun working on the functionality in Public.cshtml.cs

--- src/Chirp.Razor/CheepService.cs
+++ src/Chirp.Razor/CheepService.cs
@@ -1,8 +1,8 @@
 using Chirp.Core;
-
 public interface ICheepService {
     public Task<List<CheepDto>> GetCheeps(int page = 1);
     public Task<List<CheepDto>> GetCheepsFromAuthor(string author, int page = 1);
+    public Task<FluentValidation.Results.ValidationResult> AddCheep(string message, string authorName);
 }
 
 public class CheepService : ICheepService {
@@ -17,5 +17,10 @@ public class CheepService : ICheepService {
     public Task<List<CheepDto>> GetCheepsFromAuthor(string author, int page) {
         // filter by the provided author name
         return _cheepRepository.GetCheeps(page, author);
+    }
+
+    //validationresult not same type as in cheepRepository?? casting as quick fix
+    public Task<FluentValidation.Results.ValidationResult> AddCheep(string message, string authorName) {
+        return (Task<FluentValidation.Results.ValidationResult>)_cheepRepository.AddCheep(message, authorName);
     }
 }
--- src/Chirp.Razor/Pages/Public.cshtml.cs
+++ src/Chirp.Razor/Pages/Public.cshtml.cs
@@ -4,8 +4,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Chirp.Razor.Pages;
 
-public class PublicModel : PageModel
-{
+public class PublicModel : PageModel {
     private readonly ICheepService _service;
 
     [FromQuery(Name = "page")]
@@ -13,16 +12,22 @@ public class PublicModel : PageModel
 
     public IEnumerable<CheepDto> Cheeps { get; set; }
 
-    public PublicModel(ICheepService service)
-    {
+    public PublicModel(ICheepService service) {
         _service = service;
         Cheeps = new List<CheepDto>(0);
     }
 
-    public async Task<ActionResult> OnGet()
-    {
+    public async Task<ActionResult> OnGet() {
         Cheeps = await _service.GetCheeps(Pageno);
         Console.WriteLine(Pageno);
         return Page();
+    }
+    //fluent validation may not be the right return type since its been casted ealier.
+    public async Task<FluentValidation.Results.ValidationResult> OnPostAsync() {
+        string message = Request.Form["Text"];
+        if (message != null) {
+            return await _service.AddCheep(message, User.Identity.Name);
+        }
+        return null;
     }
 }
