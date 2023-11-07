7cb1163d0c14774a3c55f8b14803db9c8fb641af
Emil Sondum <eson@itu.dk>
2023-11-07 14:50:34 +0100

    Fetch useremail from github
    Co-authored-by: Thøger <bros@itu.dk>

--- src/Chirp.Core/Contracts/Repositories/ICheepRepository.cs
+++ src/Chirp.Core/Contracts/Repositories/ICheepRepository.cs
@@ -4,5 +4,5 @@ using FluentValidation;
 public interface ICheepRepository {
     Task<List<CheepDto>> GetCheeps(int page, string? author = null);
 
-    Task<FluentValidation.Results.ValidationResult> AddCheep(string message, string authorName);
+    Task<FluentValidation.Results.ValidationResult> AddCheep(string message, string authorName, string email);
 }
