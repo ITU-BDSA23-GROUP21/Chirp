38abf681923f953c1ce9a1c0e3aa749509de16f6
lucas <lgul@itu.dk>
2023-11-07 13:59:19 +0100

    added cheepbox in private timeline

--- src/Chirp.Razor/Pages/UserTimeline.cshtml.cs
+++ src/Chirp.Razor/Pages/UserTimeline.cshtml.cs
@@ -4,8 +4,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Chirp.Razor.Pages;
 
-public class UserTimelineModel : PageModel
-{
+public class UserTimelineModel : PageModel {
     private readonly ICheepService _service;
 
     [FromQuery(Name = "page")]
@@ -13,15 +12,32 @@ public class UserTimelineModel : PageModel
 
     public IEnumerable<CheepDto> Cheeps { get; set; }
 
-    public UserTimelineModel(ICheepService service)
-    {
+    public UserTimelineModel(ICheepService service) {
         _service = service;
         Cheeps = new List<CheepDto>(0);
     }
 
-    public async Task<ActionResult> OnGet(string author)
-    {
+    public async Task<ActionResult> OnGet(string author) {
         Cheeps = await _service.GetCheepsFromAuthor(author, Pageno);
         return Page();
     }
+    public async Task<IActionResult> OnPostAsync() {
+        string? message = Request.Form["NewMessage"];
+        if (message != null) {
+            FluentValidation.Results.ValidationResult task = await _service.AddCheep(message, User.Identity.Name);
+            HandleClientValidation(task);
+            Cheeps = await _service.GetCheeps(Pageno);
+        }
+        return Page();
+    }
+
+    public void HandleClientValidation(FluentValidation.Results.ValidationResult task) {
+        if (!task.IsValid) {
+            Console.WriteLine("Not a valid message");
+        }
+        else {
+            Console.WriteLine("Valid");
+        }
+    }
 }
+
