5524208719d1ceb5d1a6487645474896652fcb79
Thøger Bro <bros@itu.dk>
2023-11-08 16:30:33 +0100

    clean up imports

--- src/Chirp.Core/Contracts/Repositories/ICheepRepository.cs
+++ src/Chirp.Core/Contracts/Repositories/ICheepRepository.cs
@@ -1,8 +1,9 @@
+using FluentValidation.Results;
+
 namespace Chirp.Core;
-using FluentValidation;
 
 public interface ICheepRepository {
     Task<List<CheepDto>> GetCheeps(int page, string? author = null);
 
-    Task<FluentValidation.Results.ValidationResult> AddCheep(string message, string authorName, string email);
+    Task<ValidationResult> AddCheep(string message, string authorName, string email);
 }
