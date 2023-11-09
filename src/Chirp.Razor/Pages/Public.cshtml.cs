82b39946a5c350dcb22b95ab108fe37951d20760
Rafael <rafj@itu.dk>
2023-11-09 12:34:29 +0100

    Fixed merge mistakes

--- src/Chirp.Razor/Pages/Public.cshtml.cs
+++ src/Chirp.Razor/Pages/Public.cshtml.cs
@@ -1,41 +1,9 @@
 ﻿using Chirp.Core;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-using System.ComponentModel;
-using FluentValidation;
-using System.Security.Claims;
 
 namespace Chirp.Razor.Pages;
 
 public class PublicModel : TimelineModel {
-    public PublicModel(ICheepService service): base(service) {
-
-    [FromQuery(Name = "page")]
-    public int Pageno { get; set; }
-
-    public IEnumerable<CheepDto> Cheeps { get; set; }
-
-    public PublicModel(ICheepService service) {
-        _service = service;
-        Cheeps = new List<CheepDto>(0);
-    }
-
-    public async Task<ActionResult> OnGet() {
-        if(Pageno <= 0) Pageno = 1;
-        Cheeps = await _service.GetCheeps(Pageno);
-        return Page();
-    }
-    //fluent validation may not be the right return type since its been casted ealier.
-    public async Task<IActionResult> OnPostAsync() {
-        string? message = Request.Form["NewMessage"];
-        if (message != null) {
-            var email = User.Claims.Where(c => c.Type == "emails").Single().Value;
-            FluentValidation.Results.ValidationResult task = await _service.AddCheep(message, User.Identity.Name, email);
-            HandleClientValidation(task);
-            Cheeps = await _service.GetCheeps(Pageno);
-        }
-        return Page();
-    }
+    public PublicModel(ICheepService service): base(service) {}
     
     protected override Task<List<CheepDto>> GetCheeps() {
         return _service.GetCheeps(Pageno);
