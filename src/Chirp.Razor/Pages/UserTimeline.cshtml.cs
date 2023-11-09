136d07259099ca3d57ae370f170f24dff0d2da2d
Thøger Bro <bros@itu.dk>
2023-11-09 12:43:58 +0100

    disable cheeping from other timeline + clean up html

--- src/Chirp.Razor/Pages/UserTimeline.cshtml.cs
+++ src/Chirp.Razor/Pages/UserTimeline.cshtml.cs
@@ -3,10 +3,11 @@
 namespace Chirp.Razor.Pages;
 
 public class UserTimelineModel : TimelineModel {
-    public UserTimelineModel(ICheepService service) : base(service) {}
+    public UserTimelineModel(ICheepService service) : base(service) { }
 
     protected override Task<List<CheepDto>> GetCheeps() {
         // We access route data directly here, as saving it as a property in OnGet, did not make it available in OnPost
+        // Is this the right way to access route data?
         return _service.GetCheepsFromAuthor(RouteData?.Values?["author"]?.ToString(), Pageno);
     }
 }
