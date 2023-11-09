cb14796629fd7e1c0b2a1da64c71d06bfd79677c
Rafael <rafj@itu.dk>
2023-11-09 13:11:45 +0100

    Fixed offset error

--- src/Chirp.Razor/Pages/Public.cshtml.cs
+++ src/Chirp.Razor/Pages/Public.cshtml.cs
@@ -6,6 +6,7 @@ public class PublicModel : TimelineModel {
     public PublicModel(ICheepService service): base(service) {}
     
     protected override Task<List<CheepDto>> GetCheeps() {
+        if(Pageno <= 0) Pageno = 1;
         return _service.GetCheeps(Pageno);
     }
 }
--- src/Chirp.Razor/Pages/UserTimeline.cshtml.cs
+++ src/Chirp.Razor/Pages/UserTimeline.cshtml.cs
@@ -6,6 +6,7 @@ public class UserTimelineModel : TimelineModel {
     public UserTimelineModel(ICheepService service) : base(service) {}
 
     protected override Task<List<CheepDto>> GetCheeps() {
+        if(Pageno <= 0) Pageno = 1;
         // We access route data directly here, as saving it as a property in OnGet, did not make it available in OnPost
         return _service.GetCheepsFromAuthor(RouteData?.Values?["author"]?.ToString(), Pageno);
     }
