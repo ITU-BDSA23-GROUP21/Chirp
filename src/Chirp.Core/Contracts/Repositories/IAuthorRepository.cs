8ae6714346aa35331e88a6c86ff094f159a89358
Thøger Bro <bros@itu.dk>
2023-11-21 12:04:15 +0100

    start adding follow functionality in back-end

--- src/Chirp.Core/Contracts/Repositories/IAuthorRepository.cs
+++ src/Chirp.Core/Contracts/Repositories/IAuthorRepository.cs
@@ -4,4 +4,9 @@ public interface IAuthorRepository {
     Task<AuthorDto> GetAuthorByName(string name);
     Task<AuthorDto> GetAuthorByEmail(string email);
     Task CreateAuthor(AuthorDto author);
+    Task Follow(string followerName, string followingName);
+
+    Task UnFollow(string followerName, string followingName);
+
+    Task<IEnumerable<AuthorDto>> GetFollowings(string name);
 }
