9cbf3ece5282f999ba61623778d07fbb800cc6fe
Thøger Bro <bros@itu.dk>
2023-11-21 11:08:37 +0100

    add followers to data model

--- src/Chirp.Infrastructure/Entities/Author.cs
+++ src/Chirp.Infrastructure/Entities/Author.cs
@@ -4,4 +4,6 @@ public class Author {
     public required string Email { get; set; }
     public required string Name { get; set; }
     public required IEnumerable<Cheep> Cheeps { get; set; }
+    public IEnumerable<Author>? Followers { get; set; }
+    public IEnumerable<Author>? Following { get; set; }
 }
