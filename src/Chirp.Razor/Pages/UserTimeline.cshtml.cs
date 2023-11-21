42aaf7512b354d3516046360231e93d0a623ea27
Emil Sondum <eson@itu.dk>
2023-11-21 13:16:13 +0100

    Usertime following UI made
    Co-authored-by: Thøger <bros@itu.dk>

--- src/Chirp.Razor/Pages/UserTimeline.cshtml.cs
+++ src/Chirp.Razor/Pages/UserTimeline.cshtml.cs
@@ -1,4 +1,5 @@
 ﻿using Chirp.Core;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace Chirp.Razor.Pages;
 
@@ -8,7 +9,14 @@ public class UserTimelineModel : TimelineModel {
     protected override Task<List<CheepDto>> GetCheeps() {
         // We access route data directly here, as saving it as a property in OnGet, did not make it available in OnPost
         // Is this the right way to access route data?
-        return _cheepService.GetCheepsFromAuthor(RouteData?.Values?["author"]?.ToString(), Pageno);
+
+        if (User.Identity.Name == RouteData?.Values?["author"]?.ToString()) {
+            List<AuthorDto> followings = this.Followings.ToList();
+            return _cheepService.GetCheepsFromAuthors(followings, Pageno);
+        }
+        else {
+            return _cheepService.GetCheepsFromAuthor(RouteData?.Values?["author"]?.ToString(), Pageno);
+        }
     }
 }
 
