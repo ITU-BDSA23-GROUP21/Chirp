07c1f8c51fa39ecb8325bf819a86fa4b489e5cbc
lucas <lgul@itu.dk>
2023-11-23 13:36:15 +0100

    fixed unfollow

--- src/Chirp.Infrastructure/Repositories/AuthorRepository.cs
+++ src/Chirp.Infrastructure/Repositories/AuthorRepository.cs
@@ -29,7 +29,8 @@ public class AuthorRepository : IAuthorRepository {
             Name = author.Name,
             Cheeps = new List<Cheep>(),
             Followers = new List<Author>(),
-            Following = new List<Author>() }
+            Following = new List<Author>()
+        }
         );
         _dbContext.SaveChanges();
     }
@@ -64,10 +65,12 @@ public class AuthorRepository : IAuthorRepository {
     public async Task Follow(string followerName, string followingName) {
         var followingAuthor = await _dbContext.Authors
             .Where(author => author.Name == followingName)
+            .Include(author => author.Followers)
             .FirstAsync();
 
         var followerAuthor = await _dbContext.Authors
             .Where(author => author.Name == followerName)
+            .Include(author => author.Followers)
             .FirstAsync();
 
         if (followingAuthor == null || followerAuthor == null) {
@@ -83,12 +86,14 @@ public class AuthorRepository : IAuthorRepository {
     public async Task UnFollow(string followerName, string followingName) {
         var followingAuthor = await _dbContext.Authors
             .Where(author => author.Name == followingName)
+            .Include(author => author.Followers)
             .FirstAsync();
 
         var followerAuthor = await _dbContext.Authors
             .Where(author => author.Name == followerName)
+            .Include(author => author.Followers)
             .FirstAsync();
-    
+
         if (followingAuthor == null || followerAuthor == null) {
             throw new ArgumentException("Follower or following does not exist");
         }
