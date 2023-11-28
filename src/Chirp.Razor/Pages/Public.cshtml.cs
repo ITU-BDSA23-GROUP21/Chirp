ee45a847ee45d278f33bf3af3161ad8a8de89b39
lucas <lgul@itu.dk>
2023-11-28 13:13:27 +0100

    small frontend change

--- src/Chirp.Razor/Pages/Public.cshtml.cs
+++ src/Chirp.Razor/Pages/Public.cshtml.cs
@@ -3,11 +3,16 @@ using Chirp.Core;
 namespace Chirp.Razor.Pages;
 
 public class PublicModel : TimelineModel {
-    public PublicModel(ICheepService cheepService, IAuthorService authorService): base(cheepService, authorService) {
+    public PublicModel(ICheepService cheepService, IAuthorService authorService) : base(cheepService, authorService) {
 
     }
     protected override Task<List<CheepDto>> GetCheeps() {
         return _cheepService.GetCheeps(Pageno);
+    }
+
+    public void Anonymize() {
+        Console.WriteLine("hey");
+        _authorService.Anonymize("Jacqualine Gilcoine");
     }
 }
 
