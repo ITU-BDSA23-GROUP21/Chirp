2dd59f5a59097b69849e146fb4519e075124d0e6
Tobias Dirchsen Engbo <toen@itu.dk>
2023-11-28 11:31:25 +0100

    Added Followers Postgres migration

---
+++ src/Chirp.PostgresMigrations/Migrations/20231128103032_Followers.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Chirp.PostgresMigrations.Migrations
+{
+    /// <inheritdoc />
+    public partial class Followers : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AuthorAuthor",
+                columns: table => new
+                {
+                    FollowersId = table.Column<Guid>(type: "uuid", nullable: false),
+                    FollowingId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AuthorAuthor", x => new { x.FollowersId, x.FollowingId });
+                    table.ForeignKey(
+                        name: "FK_AuthorAuthor_Authors_FollowersId",
+                        column: x => x.FollowersId,
+                        principalTable: "Authors",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_AuthorAuthor_Authors_FollowingId",
+                        column: x => x.FollowingId,
+                        principalTable: "Authors",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AuthorAuthor_FollowingId",
+                table: "AuthorAuthor",
+                column: "FollowingId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AuthorAuthor");
+        }
+    }
+}
