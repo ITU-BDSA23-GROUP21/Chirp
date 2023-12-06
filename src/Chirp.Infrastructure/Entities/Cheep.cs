34eccdf8e4e3391820ef4e1223972974a2c49403
Emil Sondum <eson@itu.dk>
2023-12-06 10:40:54 +0100

    Likes entity created
    Co-authored-by: Thøger <Bros@itu.dk>

--- src/Chirp.Infrastructure/Entities/Cheep.cs
+++ src/Chirp.Infrastructure/Entities/Cheep.cs
@@ -6,4 +6,6 @@ public class Cheep {
     public required string Text { get; set; }
     public DateTime TimeStamp { get; set; }
     public required Author Author {get; set;}
+    public ICollection<Likes> Likes { get; set;}
 }
+
