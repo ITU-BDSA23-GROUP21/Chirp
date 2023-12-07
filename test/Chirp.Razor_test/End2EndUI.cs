c3d78fd4f3476fe329d33a3792e9cc26e1fc64f8
Rafael <rafj@itu.dk>
2023-12-07 12:41:34 +0100

    Added beginning of tests

--- test/Chirp.Razor_test/End2EndUI.cs
+++ test/Chirp.Razor_test/End2EndUI.cs
@@ -1,17 +1,29 @@
+using Microsoft.Playwright.NUnit;
 using NUnit.Framework;
 
 namespace Chirp.Razor_test;
 
-public class Tests
+public class Tests : PageTest
 {
+    private string ipaddress;
+
+    [OneTimeSetUp]
+    public void OnetimeSetup()
+    {
+        ipaddress = Environment.GetEnvironmentVariable("IPADDRESS") ?? string.Empty;
+    }
+
     [SetUp]
     public void Setup()
     {
     }
 
     [Test]
-    public void Test1()
+    public async Task Test1()
     {
-        Assert.Pass();
+        await Page.GotoAsync(ipaddress);
+        
+        var publicTimeline = Page.GetByRole(Microsoft.Playwright.AriaRole.Link, new(){ Name = "public timeline"});
+        await Expect(publicTimeline).ToHaveAttributeAsync("href", "/");
     }
 }
