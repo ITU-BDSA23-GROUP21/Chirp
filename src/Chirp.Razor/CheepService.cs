53894055f165096ea0c953c3e72a60de5cdf78b4
Emil Sondum <eson@itu.dk>
2023-12-07 14:19:57 +0100

    Deleting of duplicate method

--- src/Chirp.Razor/CheepService.cs
+++ src/Chirp.Razor/CheepService.cs
@@ -7,9 +7,8 @@ namespace Chirp.Razor;
 
 public interface ICheepService {
     public Task<List<CheepDto>> GetCheeps(int page = 1, string? userEmail = null);
-    public Task<List<CheepDto>> GetCheepsFromAuthor(string? author, int page = 1, string? userEmail = null);
-    public Task<ValidationResult> AddCheep(string message, string authorName, string email);
     public Task<List<CheepDto>> GetCheepsFromAuthors(IEnumerable<string> authors, int page = 1, string? userEmail = null);
+    public Task<ValidationResult> AddCheep(string message, string authorName, string email);
     public Task LikeCheep(string userEmail, string cheepId, bool like);
     public Task RemoveLike(string userEmail, string cheepId);
 }
@@ -20,26 +19,12 @@ public class CheepService : ICheepService {
     public CheepService(ICheepRepository cheepRepository) => _cheepRepository = cheepRepository;
 
     public Task<List<CheepDto>> GetCheeps(int page, string? userEmail = null) {
+        return GetCheepsFromAuthors(Enumerable.Empty<string>(), page, userEmail);
+    }
+
+    public Task<List<CheepDto>> GetCheepsFromAuthors(IEnumerable<string> authors, int page, string? userEmail = null) {
         if (page <= 0) page = 1;
-        return _cheepRepository.GetCheeps(page, Enumerable.Empty<string>(), userEmail);
-    }
-
-    public Task<List<CheepDto>> GetCheepsFromAuthor(string? author, int page, string? userEmail = null) {
-        // filter by the provided author name
-        if (author == null) {
-            throw new ArgumentNullException(nameof(author));
-        }
-
-        return _cheepRepository.GetCheeps(page, new List<string> { author }, userEmail);
-    }
-
-    public async Task<List<CheepDto>> GetCheepsFromAuthors(IEnumerable<string> authors, int page, string? userEmail = null) {
-        if (!authors.Any()) {
-            // Should we just get all cheeps instead of throwing an exception?
-            throw new ArgumentException("No authors supplied", nameof(authors));
-        }
-
-        return await _cheepRepository.GetCheeps(page, authors, userEmail);
+        return _cheepRepository.GetCheeps(page, authors, userEmail);
     }
 
     public async Task<ValidationResult> AddCheep(string message, string authorName, string email) {
