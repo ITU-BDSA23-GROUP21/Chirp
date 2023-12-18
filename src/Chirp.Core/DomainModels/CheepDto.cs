6c62b7e78afbe81c09140511754956b45bcb6c6d
Thøger Bro <bros@itu.dk>
2023-12-18 01:50:16 +0100

    update comments & remove unused code

--- src/Chirp.Core/DomainModels/CheepDto.cs
+++ src/Chirp.Core/DomainModels/CheepDto.cs
@@ -1,7 +1,7 @@
 ﻿namespace Chirp.Core;
 
-// id is now included in dto, since it avoids some expensive and imprecise operations in the repositories,
-// as the frontend can now request to like a cheep by id, instead of by author / message / timestamp
+// Id is included in dto, since it avoids some unnecessary complexity in the repositories
+// as the frontend can now request to like a cheep by id, instead of by (author, message, timestamp)
 
 // LikedByUser is true if liked, false if disliked, and null if neither
 public record CheepDto(string Id, string Author, string Message, string Timestamp, int LikeCount, bool? LikedByUser);
