dce521c7661e649a0259d788ee05fd97ba501f25
Emil Sondum <eson@itu.dk>
2023-12-18 14:17:02 +0100

    Started working of in-code documentation

--- src/Chirp.Core/DomainModels/CheepDto.cs
+++ src/Chirp.Core/DomainModels/CheepDto.cs
@@ -4,4 +4,13 @@
 // as the frontend can now request to like a cheep by id, instead of by (author, message, timestamp)
 
 // LikedByUser is true if liked, false if disliked, and null if neither
+/// <summary>
+/// Stores and transfers the necessary data of a cheep from the database to the frontend.
+/// </summary>
+/// <param name="Id">Id of the cheep.</param>
+/// <param name="Author">Name of the author that this cheep is written by.</param>
+/// <param name="Message">The context of the cheep.</param>
+/// <param name="Timestamp">The time which the cheep was made.</param>
+/// <param name="LikeCount">Combination of likes and dislikes the cheep has, expresed in a single number.</param>
+/// <param name="LikedByUser">Used to check if the cheep is liked by the authorized user. Is true if liked, false if disliked, and null if neither.</param>
 public record CheepDto(string Id, string Author, string Message, string Timestamp, int LikeCount, bool? LikedByUser);
--- src/Chirp.Infrastructure/Entities/Like.cs
+++ src/Chirp.Infrastructure/Entities/Like.cs
@@ -1,10 +1,23 @@
 namespace Chirp.Infrastructure;
-
+/// <summary>
+/// Entity that reprecent a like or dislike given to a cheep by an author.
+/// </summary>
 public class Like {
+
+    /// <value> Property <c>CheepId</c> reprecents the id of the cheep that has been liked or disliked</value>
     public Guid CheepId {get; set; }
+
+    /// <value> Property <c>AuthorId</c> reprecents the id of the author who liked or disliked</value>
     public Guid AuthorId {get; set; }
 
+    /// <value> Property <c>Cheep</c> is a reference to the cheep entity that has been liked or disliked</value>
     public required Cheep Cheep {get; set;}
+
+    /// <value> Property <c>Author</c> is a reference to the author entity who liked or disliked</value>
     public required Author Author {get; set;}
+
+    /// <value> Property <c>Liked</c> reprecent if the a cheep is liked or disliked 
+    /// <para> Is true if liked, false if disliked, and null if neither.</para>
+    /// </value>
     public required bool Liked {get; set; }
 }
