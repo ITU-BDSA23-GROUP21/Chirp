59736a26a4e02997fe8e1263156b079729357e4b
Emil Sondum <eson@itu.dk>
2023-12-18 17:38:30 +0100

    Continues work of in-code documentation Co-authored-by Tobias <toen@itu.dk> Co-authored-by Thøger <bros@itu.dk>

--- src/Chirp.Core/DomainModels/CheepDto.cs
+++ src/Chirp.Core/DomainModels/CheepDto.cs
@@ -3,7 +3,6 @@
 // Id is included in dto, since it avoids some unnecessary complexity in the repositories
 // as the frontend can now request to like a cheep by id, instead of by (author, message, timestamp)
 
-// LikedByUser is true if liked, false if disliked, and null if neither
 /// <summary>
 /// Stores and transfers the necessary data of a cheep from the database to the frontend.
 /// </summary>
@@ -11,6 +10,6 @@
 /// <param name="Author">Name of the author that this cheep is written by.</param>
 /// <param name="Message">The context of the cheep.</param>
 /// <param name="Timestamp">The time which the cheep was made.</param>
-/// <param name="LikeCount">Combination of likes and dislikes the cheep has, expresed in a single number.</param>
+/// <param name="LikeCount">Combination of likes and dislikes the cheep has, expressed in a single number.</param>
 /// <param name="LikedByUser">Used to check if the cheep is liked by the authorized user. <para>Is true if liked, false if disliked, and null if neither.</para> </param>
 public record CheepDto(string Id, string Author, string Message, string Timestamp, int LikeCount, bool? LikedByUser);
