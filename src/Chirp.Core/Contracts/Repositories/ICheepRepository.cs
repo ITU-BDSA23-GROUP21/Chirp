e0af032d8a42bad6b3bb2c597fca0775330902b9
Emil Sondum <eson@itu.dk>
2023-12-18 19:09:14 +0100

    changes to parameternames in authorservice Co-authored-by: Thøger <bros@itu.dk

--- src/Chirp.Core/Contracts/Repositories/ICheepRepository.cs
+++ src/Chirp.Core/Contracts/Repositories/ICheepRepository.cs
@@ -13,11 +13,18 @@ public interface ICheepRepository {
     /// <param name="page">The page number </param>
     /// <param name="authors">A list of author names used to find cheeps made by specific authors</param>
     /// <param name="userEmail">Email of the current user</param>
-    /// <returns></returns>
+    /// <returns>A task containing a list of CheepDtos </returns>
     Task<List<CheepDto>> GetCheeps(int page, IEnumerable<string> authors, string? userEmail = null);
 
     Task<ValidationResult> AddCheep(string message, string authorName);
 
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="userEmail">Email of the current user</param>
+    /// <param name="cheepId">Id of the cheep thats liked or disliked</param>
+    /// <param name="value"></param>
+    /// <returns>A task that represents the asynchronous operation</returns>
     Task LikeCheep(string userEmail, string cheepId, bool value);
 
     Task RemoveLike(string userEmail, string cheepId);
