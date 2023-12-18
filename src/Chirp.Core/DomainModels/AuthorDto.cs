af8f521e421c32b6381dd73ee97d8f74e57af552
Emil Sondum <eson@itu.dk>
2023-12-18 14:17:02 +0100

    Started working of in-code documentation

--- src/Chirp.Core/DomainModels/AuthorDto.cs
+++ src/Chirp.Core/DomainModels/AuthorDto.cs
@@ -1,2 +1,7 @@
 namespace Chirp.Core;
+/// <summary>
+/// Values from an author entity needed by the frontend.
+/// </summary>
+/// <param name="Name">Name of the author</param>
+/// <param name="Email">Email of the author</param>
 public record AuthorDto(string Name, string Email);
--- src/Chirp.Infrastructure/Entities/Author.cs
+++ src/Chirp.Infrastructure/Entities/Author.cs
@@ -1,13 +1,36 @@
 namespace Chirp.Infrastructure;
+/// <summary>
+/// Entity that represents a user in the system.
+/// <para> Authors can write, like and dislike cheeps, aswell as follow and be followed by other authors.</para>
+/// </summary>
 public class Author {
-    public Guid Id { get; set; }
-    public required string Email { get; set; }
-    public required string Name { get; set; }
-    public required IEnumerable<Cheep> Cheeps { get; set; }
-    public required ICollection<Author> Followers { get; set; }
-    public required ICollection<Author> Following { get; set; }
-    public ICollection<Like> Likes {get; set; }
 
+    /// <value> Property <c>Id</c> is a global unique identifier, created by the datatype 
+    /// <seealso cref="Guid " href=" https://learn.microsoft.com/en-us/dotnet/api/system.guid?view=net-8.0"/>
+    /// </value>
+    public Guid Id { get; set; }
+
+    /// <value> Property <c>Email</c> represents the email of the author </value>
+    public required string Email { get; set; }
+
+    /// <value>Property <c>Name</c> represents the name of the author </value>
+    public required string Name { get; set; }
+
+    /// <value> Property <c>Cheeps</c> is a collection of the cheeps created by the author </value>
+    public required IEnumerable<Cheep> Cheeps { get; set; }
+
+    /// <value> Property <c>Followers</c> is a collection of authors that follows this author </value>
+    public required ICollection<Author> Followers { get; set; }
+
+    /// <value> Property <c>Following</c> is a collection of authors that this author follows </value>
+    public required ICollection<Author> Following { get; set; }
+
+    /// <value> Property <c>Likes</c> is a collection of like entities</value>
+    public ICollection<Like> Likes {get; set; }
+    
+    /// <summary>
+    /// This constructor initalizes the <see cref="Likes"/> collection to a new list.
+    /// </summary>
     public Author() {
         Likes = new List<Like>();
     }
