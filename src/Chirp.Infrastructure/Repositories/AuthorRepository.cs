d7eda52cdc32690a779cd96639c9073f7b3e0961
Emil Sondum <eson@itu.dk>
2023-12-20 00:02:56 +0100

    More documentation

--- src/Chirp.Infrastructure/Repositories/AuthorRepository.cs
+++ src/Chirp.Infrastructure/Repositories/AuthorRepository.cs
@@ -3,7 +3,11 @@ using FluentValidation;
 using Microsoft.EntityFrameworkCore;
 
 namespace Chirp.Infrastructure;
-
+/// <summary>
+/// This class implements the methods from the interfaces. And it dependents on <c>ChirpContext</c>
+/// <para> It uses the dependency to access the database 
+/// to get, add and alter cheeps</para>
+/// </summary>
 public class AuthorRepository : IAuthorRepository {
     private readonly ChirpContext _dbContext;
     private readonly AuthorValidator _authorValidator = new();
