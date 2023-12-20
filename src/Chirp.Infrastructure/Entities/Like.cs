7f84e40d181f19359523ef7a731a1b22f116cc77
Emil Sondum <eson@itu.dk>
2023-12-20 12:03:45 +0100

    Like entity documentation updated

--- src/Chirp.Infrastructure/Entities/Like.cs
+++ src/Chirp.Infrastructure/Entities/Like.cs
@@ -3,16 +3,16 @@ namespace Chirp.Infrastructure;
 /// Entity that represent a like or dislike given to a cheep by an author.
 /// </summary>
 public class Like {
-    public Guid CheepId {get; set; }
-    public Guid AuthorId {get; set; }
+    public Guid CheepId { get; set; }
+    public Guid AuthorId { get; set; }
     /// <summary> Needed for Ef core to know <c>CheepId</c> references a cheep in the database</summary>
-    public required Cheep Cheep {get; set;}
+    public required Cheep Cheep { get; set; }
     /// <summary> Needed for Ef core to know <c>AuthorId</c> references an author in the database </summary>
-    public required Author Author {get; set;}
-    /// <summary> Is true if a cheep is liked, false if it is disliked, and null if neither
+    public required Author Author { get; set; }
+    /// <summary> Is true if a cheep is liked, false if it is disliked
     ///
     /// <para> This should be remade, so it is an enum of reactions instead of a bool. 
-    /// This way null is not used as a value, and it would be extendable to create more reaction types</para>
+    /// This way null is not used as a value in the <c> CheepDTO </c>, and it would be extendable to create more reaction types</para>
     /// </summary>
-    public required bool Liked {get; set; }
+    public required bool Liked { get; set; }
 }
